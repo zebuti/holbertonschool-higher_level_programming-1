5b0dc4f51f21b9b99f32f93621119eced4e6ccc0
kai-dg <km.commissions@gmail.com>
2018-12-05 23:08:43 +0000

    fixed 1 arg edge case

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,14 @@ if __name__ == "__main__":
     import sys
     argv = sys.argv[1:]
     argv_count = len(argv)
-    if argv_count < 1:
+    index = 1
+    if argv_count is 0:
+        print("{:d} arguments.".format(argv_count))
+    elif argv_count is 1:
         print("{:d} argument.".format(argv_count))
+        print("{:d}: {:s}".format(index, sys.argv[1]))
     else:
         print("{:d} arguments:".format(argv_count))
-        index = 1
         while index <= argv_count:
             print("{:d}: {:s}".format(index, sys.argv[index]))
             index += 1
