23a0c1b3ca0f69b79c11db5715a856cb8432e660
kai-dg <km.commissions@gmail.com>
2018-12-05 01:18:27 +0000

    changed to char format in print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    res = ""
     for i in str:
         i = ord(i)
         if 97 <= i <= 122:
             i = i - 32
-        res += chr(i)
-    print(res)
+        print("{:c}".format(i), end='')
+    print()
