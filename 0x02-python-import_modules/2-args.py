bcfe286c05c53947a59a00189d6187996dd7f8ef
kai-dg <km.commissions@gmail.com>
2018-12-05 23:10:46 +0000

    fixed semicolon in 1 arg case

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if argv_count is 0:
         print("{:d} arguments.".format(argv_count))
     elif argv_count is 1:
-        print("{:d} argument.".format(argv_count))
+        print("{:d} argument:".format(argv_count))
         print("{:d}: {:s}".format(index, sys.argv[1]))
     else:
         print("{:d} arguments:".format(argv_count))
