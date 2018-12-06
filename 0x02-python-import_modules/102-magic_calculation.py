81121a5d3a83e1088972653c2ff1027dffe45887
kai-dg <km.commissions@gmail.com>
2018-12-06 00:22:01 +0000

    removed dis functions

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import dis
 
 
 def magic_calculation(a, b):
@@ -11,4 +10,3 @@ def magic_calculation(a, b):
         return (c)
     else:
         return (sub(a, b))
-dis.dis(magic_calculation)
