97de318b45a39f1a71b6c45c88e88092610ce409
kai-dg <km.commissions@gmail.com>
2018-12-06 00:21:18 +0000

    added else statement for correct >>

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import dis
+
+
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
@@ -6,5 +9,6 @@ def magic_calculation(a, b):
         for i in range(4, 6):
             c = add(c, i)
         return (c)
+    else:
         return (sub(a, b))
-# dis.dis(magic_calculation)
+dis.dis(magic_calculation)
