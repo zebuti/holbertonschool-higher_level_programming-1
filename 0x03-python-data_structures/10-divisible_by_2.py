7619792b1217f97fd5826c9693b019e003710e96
kai-dg <km.commissions@gmail.com>
2018-12-11 22:24:28 +0000

    adding the rest of python mandatories

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list == None:
+        return None
+    new_list = list(my_list)
+    for index in new_list:
+        if index % 2 == 0:
+            new_list[index] = True
+        else:
+            new_list[index] = False
+    return new_list
