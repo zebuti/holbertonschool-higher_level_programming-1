f3378c6b1789606e991df64ecc1d4bcb91310cc5
kai-dg <km.commissions@gmail.com>
2018-12-11 22:29:31 +0000

    fixing pep8

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list == None:
+    if not my_list:
         return None
     new_list = list(my_list)
     for index in new_list:
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == None:
+    if not my_list:
         return None
     biggest = 0
     for index in my_list:
