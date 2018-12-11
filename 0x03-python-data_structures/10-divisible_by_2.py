8201545a5040caaad3f14f8b7f407868053b2458
kai-dg <km.commissions@gmail.com>
2018-12-11 22:31:15 +0000

    testing none edge case

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     if not my_list:
-        return None
+        return my_list
     new_list = list(my_list)
     for index in new_list:
         if index % 2 == 0:
