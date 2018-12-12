6e196959538e3f61a3c68094878a21837a6c3586
kai-dg <km.commissions@gmail.com>
2018-12-12 05:52:45 +0000

    changed logic of if statements

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,6 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    if idx > len(my_list):
         return None
-    else:
-        for index in my_list:
-            if index == idx:
-                return my_list[idx]
+    return my_list[idx]
