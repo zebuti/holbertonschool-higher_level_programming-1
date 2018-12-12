a6a8bc432022f58712612fa111af62d51f7914cd
kai-dg <km.commissions@gmail.com>
2018-12-12 06:06:32 +0000

    changed logic and added equal to len case

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,11 +2,8 @@
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    elif idx > len(my_list):
+    if idx >= len(my_list):
         return my_list
-    else:
-        temp_list = my_list[:]
-        for index in temp_list:
-            if index == idx:
-                temp_list[idx] = element
-                return temp_list
+    temp_list = list(my_list)
+    temp_list[idx] = element
+    return temp_list
