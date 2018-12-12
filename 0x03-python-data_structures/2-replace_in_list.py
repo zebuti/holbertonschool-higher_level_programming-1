efef9bf5870c35574ebc9765b3dac0394eff45ed
kai-dg <km.commissions@gmail.com>
2018-12-12 06:00:26 +0000

    changed logic and added equal to len case

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,10 +2,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    elif idx > len(my_list):
+    if idx >= len(my_list):
         return my_list
-    else:
-        for index in my_list:
-            if index == idx:
-                my_list[idx] = element
-                return my_list
+    my_list[idx] = element
+    return my_list
