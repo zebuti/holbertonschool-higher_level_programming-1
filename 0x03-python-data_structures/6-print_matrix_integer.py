626ed0b64efa254a0816fe2c941dcfe567ec1c15
kai-dg <km.commissions@gmail.com>
2018-12-13 03:03:04 +0000

    fixed case of 1, pep8, and None value

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 1:
+    if not matrix:
         print()
     else:
         for row in range(len(matrix)):
             for item in range(len(matrix[row])):
                 if item != len(matrix[row]) - 1:
-                    endspace= ' '
+                    endspace = ' '
                 else:
-                    endspace=''
+                    endspace = ''
                 print("{:d}".format(matrix[row][item]), end=endspace)
             print()
