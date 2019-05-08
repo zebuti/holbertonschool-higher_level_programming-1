9db8590079cbe74be5aa21b9939a324cd1121b42
kai-dg <km.commissions@gmail.com>
2019-05-08 04:15:19 +0000

    fixed style

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@
 let num = parseInt(process.argv[2]);
 
 if (num) {
-  console.log('My number: ' + num)
+  console.log('My number: ' + num);
 } else {
-  console.log('Not a number')
+  console.log('Not a number');
 }
