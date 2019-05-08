48e4e51f451655c6100a2d19edb4fb38a3ef1c82
kai-dg <km.commissions@gmail.com>
2019-05-08 14:51:04 +0000

    fixed char output

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ let num = parseInt(process.argv[2]);
 
 if (num) {
   for (let i = 0; i < num; i++) {
-    console.log('x'.repeat(num));
+    console.log('X'.repeat(num));
   }
 } else if (num < 0) {
   console.log('');
