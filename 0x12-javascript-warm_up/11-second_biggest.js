00a5d0caf9ae1d789caab5a24806a4b6e5b2dbc4
kai-dg <km.commissions@gmail.com>
2019-05-08 14:57:25 +0000

    added number sort function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 // JS Script
 
+function sortNumber(a, b) {
+  return a - b;
+}
 let argsLen = process.argv.length;
 if (argsLen === 2 || argsLen === 3) {
   console.log('0');
@@ -9,5 +12,7 @@ if (argsLen === 2 || argsLen === 3) {
   for (let i = 2; i < argsLen; i++) {
     arr.push(process.argv[i]);
   }
-  console.log(arr.sort()[arr.length - 2]);
+  arr.sort(sortNumber);
+  console.log(arr[arr.length - 2]);
+  console.log(arr)
 }
